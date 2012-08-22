de9629fb9df9f2b86a10d38710fcf7999d591f2d
Dipesh Patel <dipthegeezer.opensource@gmail.com>
2012-08-22 17:38:36 +0100

    Basic migration creation function

--- src/erlsqlmigrate.erl
+++ src/erlsqlmigrate.erl
@@ -1,37 +1,99 @@
 -module(erlsqlmigrate).
 
--export([main/1]).
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
 
-%% ====================================================================
-%% Public API
-%% ====================================================================
+-export([main/1]).
+-export([create/3,up/3,down/3]).
+
+%% ------------------------------------------------------------------
+%% Record Definitions
+%% ------------------------------------------------------------------
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
 main([]) ->
     usage();
 
 main(Args) ->
-    case catch(run(Args)) of
-        ok ->
-            ok;
-        Error ->
-            io:format("Uncaught error: ~p\n", [Error])
+    OptSpecList = option_spec_list(),
+    case getopt:parse(OptSpecList, Args) of
+        {ok, {Options, NonOptArgs}} ->
+            case parse_command_args(NonOptArgs) of
+                {Cmd,Name} ->
+                    case run(Options, Cmd, Name) of
+                        ok ->
+                            ok;
+                        Error ->
+                            io:format("Uncaught error: ~p\n", [Error])
+                    end;
+                error -> usage(OptSpecList)
+            end;
+        {error, {Reason, Data}} ->
+            io:format("Error: ~s ~p~n~n", [Reason, Data]),
+            usage(OptSpecList)
     end.
 
+create(Config, MigDir, Name) ->
+    io:format("~nConfig:~n  ~p~n~nName:~n  ~p~n ~p~n", [Config,Name,MigDir]),
+    erlsqlmigrate_core:create(Config, MigDir, Name).
+
+up(Config, MigDir, Name) ->
+    io:format("~nConfig:~n  ~p~n~nName:~n  ~p~n ~p~n", [Config,Name,MigDir]),
+    erlsqlmigrate_core:up().
+
+down(Config, MigDir, Name) ->
+    io:format("~nConfig:~n  ~p~n~nName:~n  ~p~n ~p~n", [Config,Name,MigDir]),
+    erlsqlmigrate_core:down().
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
 usage() ->
     usage(option_spec_list()).
 
 usage(OptSpecList) ->
-    getopt:usage(OptSpecList, escript:script_name(), "[up,down,create] [name]",
+    getopt:usage(OptSpecList, escript:script_name(), "[up, down, create] [name]",
                  [{"command",   "Migration command"},
                  {"name", "Optional migration name"}]).
 
 option_spec_list() ->
+    {ok, Cwd } = file:get_cwd(),
+    MigrationsDefault = filename:join([Cwd,"migrations"]),
+    ConfigDefault = filename:join([Cwd,"config"]),
+
     [
-     {help,            $h, "help",          undefined,             "Show the program options"},
-     {verbose,         $v, "verbose",       {boolean, false},      "Be verbose about what gets done"},
-     {version,         $V, "Version",       {boolean, false},      "output the version number"},
-     {migrations_dir,  $m, "migration-dir", string,                "The directory containing your SQL migration files [./migrations]"},
-     {config_dir,      $c, "config-dir",    string,                "Location of your config files [./config]"}
+     {help,            $h, "help",          undefined,                    "Show the program options"},
+     {verbose,         $v, "verbose",       {boolean, false},             "Be verbose about what gets done"},
+     {version,         $V, "Version",       {boolean, false},             "output the version number"},
+     {environment,     $e, "environment",   {string,"development"},       "Environment you are running migration on. Defaults to 'development'"},
+     {migration_dir,   $m, "migration-dir", {string, MigrationsDefault},  "The directory containing your SQL migration files [./migrations]"},
+     {config_dir,      $c, "config-dir",    {string, ConfigDefault},      "Location of your config files [./config]"}
     ].
 
-run(_Args) ->
-    ok.
+run(Options, Cmd, Name) ->
+    io:format("Options:~n  ~p~n~nNon-option arguments:~n  ~p~p~n", [Options, Cmd, Name]),
+    ConfigDir = proplists:get_value(config_dir, Options),
+    Environment = proplists:get_value(environment, Options),
+    MigDir = proplists:get_value(migration_dir, Options),
+    {ok, Config} = file:consult(filename:join([ConfigDir,Environment++".config"])),
+    Mod = ?MODULE,
+    Mod:Cmd(Config, MigDir, Name).
+
+parse_command_args(["create", Name]) ->
+    {create,Name};
+parse_command_args(["create"]) ->
+    io:format("~nMust specify a name with `create` command~n~n"),
+    error;
+parse_command_args(["down", Name]) ->
+    {down,Name};
+parse_command_args(["down"]) ->
+    io:format("~nMust specify a name with `down` command~n~n"),
+    error;
+parse_command_args(["up"]) ->
+    {up,""};
+parse_command_args(Args) ->
+    io:format("~nUnknown Commands:~p~n~n",[Args]),
+    error.
--- src/erlsqlmigrate_core.erl
+++ src/erlsqlmigrate_core.erl
@@ -1,10 +1,39 @@
 -module(erlsqlmigrate_core).
 
--export([create/0,
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([create/3,
          up/0,
          down/0]).
+%% ------------------------------------------------------------------
+%% Record Definitions
+%% ------------------------------------------------------------------
+-record(migration, { date,
+                     name,
+                     title,
+                     up_path,
+                     down_path,
+                     up,
+                     down }).
 
-create() ->
+%% ------------------------------------------------------------------
+%% Macro Definitions
+%% ------------------------------------------------------------------
+-define(UPDIR(MigDir,Driver),filename:join([MigDir,Driver,"up"])).
+-define(DOWNDIR(MigDir,Driver),filename:join([MigDir,Driver,"down"])).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+create([{Driver,_ConnArgs}]=Config,MigDir,Name) ->
+    filelib:ensure_dir(?UPDIR(MigDir,Driver)++"/"),
+    filelib:ensure_dir(?DOWNDIR(MigDir,Driver)++"/"),
+    {ok, Migration }= get_migration(Driver,MigDir,Name),
+    file:write_file(Migration#migration.up_path, <<"">>),
+    file:write_file(Migration#migration.down_path, <<"">>),
+    run_driver(Config,create,[Migration]),
     ok.
 
 up() ->
@@ -12,3 +41,26 @@ up() ->
 
 down() ->
     ok.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
+run_driver([{pgsql,ConnArgs}],Cmd,Args) ->
+    erlsqlmigrate_driver_pg:Cmd(ConnArgs,Args).
+
+%% @spec () -> integer()
+%% @doc Return the current timestamp via erlang:now().
+get_timestamp() ->
+    {Megaseconds,Seconds,Microseconds} = erlang:now(),
+    (Megaseconds*1000000+Seconds)*1000000+Microseconds.
+
+get_migration(Driver,MigDir,Name) ->
+    Timestamp = get_timestamp(),
+    Title = integer_to_list(Timestamp)++"-"++Name++".sql",
+    {ok, #migration{date=Timestamp,
+                    name=Name,
+                    title=Title,
+                    up_path=?UPDIR(MigDir,Driver)++"/"++Title,
+                    down_path=?DOWNDIR(MigDir,Driver)++"/"++Title
+                   }}.
